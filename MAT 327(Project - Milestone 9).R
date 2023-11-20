f2c46a523e86dfd2731b0ebf8ffa7e6af09801f3
Ishaka17 <145288695+Ishaka17@users.noreply.github.com>
2023-11-20 15:49:53 -0500

    Add files via upload

---
+++ MAT 327(Project - Milestone 9).R
@@ -0,0 +1,8 @@
+library(readr)
+temp <- read_csv("temp.csv")
+View(temp)
+View(temp)prop.test(c(70,91), c(74,95))
+2*pnorm(-0.30)
+qnorm(0.05/2)
+prop.test(81,85, p = 0.01, alternative = "greater")
+qnorm(0.99)
